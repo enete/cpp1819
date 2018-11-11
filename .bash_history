fbebad674bb76f90b3b3986fb1a9be9e91edc374
enete <6undeli@informatik.uni-hamburg.de>
2018-11-11 18:28:06 +0100

    2018-11-11

---
+++ .bash_history
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt list --upgradable 
+sudo apt upgrade 
+ls
+sudo apt install gcc
+sudo apt install vim
+sudo apt install vimdiff
+sudo apt install gdb
+sudo apt install git
+exit
+cd Blatt02
+ls
+gcc ./03_Debugging_exercise_02.c -Og -ggdb
+gdb ./a.out
+quit
+exit
---
+++ Blatt02/03_Routine.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*
+ *  Kleinstes gemeinsames Vielfaches
+ */
+void kgv(int n, int m)
+{
+
+}
+
+/*
+ * Größter gemeinsamer Teiler
+ */
+void ggt(int n,int m)
+{
+
+}
+
+/*
+ * Primzahl Test
+ */
+int isPrime(int eingabe)
+{
+
+}
+
+/**
+ * main
+ */
+int main(int argc, char const *argv[]) 
+{
+	// kgv
+	printf("Kleinstes gemeinsames Vielfache:\n");
+	kgv(i);
+
+	// ggt
+	printf("Größter gemeinsamer Teiler:\n");
+	ggt();
+
+	// Method03
+	printf("\nMethod03:\n");
+	for(int i; i<100; i++);
+	{
+		printf("%d ist eine Primzahl\n")isPrime(i);
+	}	
+}
